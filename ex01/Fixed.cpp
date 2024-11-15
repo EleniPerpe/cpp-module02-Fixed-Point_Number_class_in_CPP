b720f137d323a7ed06ea5f03c57fa09cd8d0a31e
Eleni Perperidou <eperperi@2-I-7.42heilbronn.de>
2024-11-15 18:39:30 +0100

    constructors ready

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 18:16:07 by eperperi          #+#    #+#             */
+/*   Updated: 2024/11/15 18:38:50 by eperperi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedPoint(0)
+{
+}
+
+Fixed::Fixed(const int intNum)
+{
+	_fixedPoint = intNum << _fractional;
+}
+
+Fixed::Fixed(const float floatNum)
+{
+	_fixedPoint = roundf(floatNum * (1 << _fractional));
+}
+
+Fixed::Fixed(const Fixed& original)
+{
+	*this = original;
+}
+
+Fixed &Fixed::operator=(const Fixed& original)
+{
+	if (this != &original)
+		this->_fixedPoint = original._fixedPoint;
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+}
