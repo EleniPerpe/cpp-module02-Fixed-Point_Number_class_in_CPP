3b8f6592dc6000af73da98e437b2a1c73771d98e
Eleni Perperidou <eperperi@2-G-3.42heilbronn.de>
2024-11-19 14:29:44 +0100

    finished

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:49:52 by eperperi          #+#    #+#             */
-/*   Updated: 2024/11/18 18:14:59 by eperperi         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:24:11 by eperperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,16 @@ public:
 
 	const Fixed& getX() const;
 	const Fixed& getY() const;
+
 };
 
 bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+// std::ostream& operator<<(std::ostream& os, const Point& point)
+// {
+//  os << std::endl;
+// 	os << point.getX();
+// 	os << std::endl;
+// 	os << point.getY();
+// 	return os;
+// }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:25:04 by eperperi          #+#    #+#             */
-/*   Updated: 2024/11/18 19:20:09 by eperperi         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:24:16 by eperperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main( void )
 	Point b(0, 10);
 	Point c(10, 5);
 
-	Point example1(4, 7);
+	Point example1(4, 5);
 	if (bsp(a, b, c, example1))
 		std::cout << "INSIDE" << std::endl;
 	else
